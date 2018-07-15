c748fcfaf0a3c9681d67c6642cc43f1baf909859
nerazzurri10 <30605796+nerazzurri10@users.noreply.github.com>
2018-07-15 23:07:37 +0300

    Three Classes

---
+++ src/main/java/com/example/WebAppProcess20/Library/Invoice.java
@@ -0,0 +1,45 @@
+package com.example.WebAppProcess20.Library;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Invoice {
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private int invoiceID;
+    private int orderId;
+
+    @Override
+    public String toString() {
+        return "Invoice{" +
+                "invoiceID=" + invoiceID +
+                ", orderId=" + orderId +
+                '}';
+    }
+
+    public int getInvoiceID() {
+        return invoiceID;
+    }
+
+    public void setInvoiceID(int invoiceID) {
+        this.invoiceID = invoiceID;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public Invoice(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public Invoice() {
+    }
+}
