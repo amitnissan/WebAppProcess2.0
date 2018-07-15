f82eff539d897cee3752862086479de903d7e761
nerazzurri10 <30605796+nerazzurri10@users.noreply.github.com>
2018-07-15 23:02:06 +0300

    Three Classes

---
+++ src/main/java/com/example/WebAppProcess20/Library/Client.java
@@ -0,0 +1,147 @@
+package com.example.WebAppProcess20.Library;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private int clientID;
+    private String clientName;
+    private Date birthDate;
+    private String homeAddress;
+    private String city;
+    private String country;
+    private String gender;
+    private String favouriteCategories;
+    private String userName;
+    private String password;
+    private String emailAddress;
+
+
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "clientID=" + clientID +
+                ", clientName='" + clientName + '\'' +
+                ", birthDate=" + birthDate +
+                ", homeAddress='" + homeAddress + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                ", gender='" + gender + '\'' +
+                ", favouriteCategories='" + favouriteCategories + '\'' +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                '}';
+    }
+
+    public int getClientID() {
+        return clientID;
+    }
+
+    public void setClientID(int clientID) {
+        this.clientID = clientID;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(String homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getFavouriteCategories() {
+        return favouriteCategories;
+    }
+
+    public void setFavouriteCategories(String favouriteCategories) {
+        this.favouriteCategories = favouriteCategories;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public Client() {
+    }
+
+    public Client(String clientName, Date birthDate, String homeAddress, String city, String country, String gender, String favouriteCategories, String userName, String password, String emailAddress) {
+        this.clientName = clientName;
+        this.birthDate = birthDate;
+        this.homeAddress = homeAddress;
+        this.city = city;
+        this.country = country;
+        this.gender = gender;
+        this.favouriteCategories = favouriteCategories;
+        this.userName = userName;
+        this.password = password;
+        this.emailAddress = emailAddress;
+    }
+}
