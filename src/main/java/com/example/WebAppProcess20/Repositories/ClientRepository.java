dd084338fcb7073624e8ba5ae07427a4f62e63b9
Amit Nissan <amitnissan20@gmail.com>
2018-07-15 23:46:07 +0300

    hi

---
+++ src/main/java/com/example/WebAppProcess20/Repositories/ClientRepository.java
@@ -0,0 +1,11 @@
+package com.example.WebAppProcess20.Repositories;
+
+/*
+ * Created by Amit Nissan on 15/7/2018
+ */
+
+import com.example.WebAppProcess20.Library.Client;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClientRepository extends CrudRepository<Client,Integer> {
+}
