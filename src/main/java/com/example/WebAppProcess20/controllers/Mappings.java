8a0d1a1c93ae163c25c85381ae81b0b2dbf50190
Amit Nissan <amitnissan20@gmail.com>
2018-08-06 21:17:49 +0300

    The Process

--- src/main/java/com/example/WebAppProcess20/controllers/Mappings.java
+++ src/main/java/com/example/WebAppProcess20/controllers/Mappings.java
@@ -4,7 +4,6 @@ package com.example.WebAppProcess20.controllers;
  * Created by Amit Nissan on 29/7/2018
  */
 
-import com.example.WebAppProcess20.Constants;
 import com.example.WebAppProcess20.Entities.ClientsEntity;
 import com.example.WebAppProcess20.Entities.ProductsEntity;
 import org.hibernate.Session;
@@ -13,6 +12,7 @@ import org.hibernate.cfg.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 
@@ -21,10 +21,28 @@ public class Mappings {
 
     @RequestMapping("/login")
     public String login(UserSignup user) {
+        return "login";
+    }
+
+    @RequestMapping("/signup")
+    public String signup(UserSignup user){
+        return "signup";
+    }
+    @RequestMapping("/signup1")
+    public String signup1(UserSignup user){
+        Configuration cfg = new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        SessionFactory factory = cfg.buildSessionFactory();
         Session session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
+        Session session_for_db = factory.openSession();
         ClientsEntity client = new ClientsEntity();
-        client.setClientId(String.valueOf(Constants.current_client_id));
+        try{
+            ArrayList<ClientsEntity> clients = (ArrayList<ClientsEntity>) session_for_db.createQuery("from ClientsEntity ").list();
+            client.setClientId(String.valueOf(Integer.parseInt(clients.get(clients.size() - 1).getClientId()) + 1));
+        }catch(NullPointerException e){
+            client.setClientId("1");
+        }
         client.setCity(user.getCity());
         client.setClientName(user.getClientName());
         client.setCountry(user.getCountry());
@@ -38,41 +56,38 @@ public class Mappings {
         session.save(client);
         session.getTransaction().commit();
         HibernateUtil.shutdown();
-        return "login";
-    }
-
-    @RequestMapping("/signup")
-    public String signup(UserSignup user){
-        return "signup";
-    }
-    @RequestMapping("/signup1")
-    public String signup1(UserSignup user){
         return "signup1";
     }
 
-    @RequestMapping("/search1")
-    public String search1(UserSearch search){
+    @RequestMapping("/results")
+    public ModelAndView results(UserSearch search){
+        ModelAndView mav = new ModelAndView("results");
         Configuration cfg = new Configuration();
         cfg.configure("hibernate.cfg.xml");
         SessionFactory factory = cfg.buildSessionFactory();
         Session session = factory.openSession();
 
         ArrayList<ProductsEntity> li = (ArrayList<ProductsEntity>)session.createQuery("from ProductsEntity").list();
+        ArrayList<ProductsEntity> res = new ArrayList<ProductsEntity>();
         for (ProductsEntity c: li) {
             if (c.getProductName().toLowerCase().contains(search.getSearch().toLowerCase())){
-                System.out.println(c.getProductName());
+                res.add(c);
             }
         }
+        mav.addObject("results",res);
         session.close();
         factory.close();
-
-        return "search1";
+        return mav;
     }
 
 
     @RequestMapping("/cart")
     public String cart (Model model){
         return "cart";
+    }
+    @RequestMapping("/search1")
+    public String search1 (Model model){
+        return "search1";
     }
 
     @RequestMapping("/search")
@@ -93,4 +108,12 @@ public class Mappings {
         return "login";
     }
 
+    @RequestMapping("/about")
+    public String about(Model model){
+        return "about";
+    }
+    @RequestMapping("/contact")
+    public String contact(Model model){
+        return "contact";
+    }
 }
